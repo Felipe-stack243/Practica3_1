6d9f551b7a3e8c218c3d0da487cd7f703475b887
Felipe-stack243 <80722743+Felipe-stack243@users.noreply.github.com>
2024-04-08 09:28:59 -0600

    Control de elementos del DOM

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Acceder al boton y al cuadro de texto
+    var cambiarColorBtn = document.getElementById("cambiarColorBtn");
+    var cuadroTexto     = document.getElementById("cuadroTexto");
+
+    // Agregar un eventListener para el click en el botón
+    cambiarColorBtn.addEventListener("click", function(){
+        // Generar un color aleatorio en formato hexadecimal
+        var color = "#"+Math.floor(Math.random()*16777215).toString(16);
+        // Cambiar el color de fondo del cuadro de texto
+        cuadroTexto.style.backgroundColor = color;
+    });
+});
